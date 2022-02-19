9bb001c414b792323074ec4955dbf63caed77f2e
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-02-18 22:12:05 -0800

    created and import Menu and Meal components. added css modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Layout/Header/Header'
+import Menu from './components/Menu/Menu'
 
 function App() {
   return (
     <div className='container'>
       <Header />
+      <Menu />
     </div>
   )
 }
