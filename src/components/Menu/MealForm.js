dddef471208055a9a37545343e219f108ec3515a
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-02-22 20:18:50 -0800

    import useReducer in MealForm component

--- src/components/Menu/MealForm.js
+++ src/components/Menu/MealForm.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useContext, useReducer } from 'react'
 import Button from '../UI/Button'
 import styles from './MealForm.module.css'
+import CartContext from '../store/cartContext'
 
 const MealForm = props => {
-  const handleAddMeal = e => {
+  const cartContext = useContext(CartContext)
+
+  const handleAddMeals = e => {
     e.preventDefault()
   }
 
   return (
     <div>
-      <form onSubmit={handleAddMeal}>
+      <form onSubmit={handleAddMeals}>
         <label htmlFor={props.key}>QTY</label>
         <input className={styles.input} id={props.key} type='number' />
         <Button type='button'>-</Button>
