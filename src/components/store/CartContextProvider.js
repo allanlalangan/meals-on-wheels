84e5d193f4bb3fd0d530d69423028ef85aa1e05b
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-02-24 14:03:11 -0800

    import useReducer in CartContextProvider

--- src/components/store/CartContextProvider.js
+++ src/components/store/CartContextProvider.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import CartContext from './cartContext'
 
+const defaultCartState = {
+  items: [],
+  totalPrice: 0,
+}
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      throw new Error()
+  }
+}
+
 const CartContextProvider = props => {
+  const [cartState, cartDispatch] = useReducer(cartReducer, defaultCartState)
+
   const handleAddItem = item => {}
   const handleRemoveItem = id => {}
 
   const cartContext = {
-    items: [],
-    totalPrice: 0,
+    items: cartState.items,
+    totalPrice: cartState.totalPrice,
     addItem: handleAddItem,
     removeItem: handleRemoveItem,
   }
